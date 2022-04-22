ac74135e9c0b600fee83af942645cc94fbbedf24
B-Bischoff <mainbischoffbrice@gmail.com>
2022-04-22 16:29:30 +0200

    adding cleaned and completed libft

---
+++ Libft/math/ft_vector_rot.c
@@ -0,0 +1,10 @@
+#include "../libft.h"
+
+t_vector2_d	rotate_vector_d(t_vector2_d vector, double radian)
+{
+	t_vector2_d	new_vect;
+
+	new_vect.x = vector.x * cos(radian) - vector.y * sin(radian);
+	new_vect.y = vector.x * sin(radian) + vector.y * cos(radian);
+	return (new_vect);
+}
