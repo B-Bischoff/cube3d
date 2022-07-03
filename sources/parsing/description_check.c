5b9f0164246fe749d8db437fbdda2f0179ef173b
Brice Bischoff <mainbischoffbrice@gmail.com>
2022-07-03 17:19:31 +0200

    Fixed inverted texture orientation

--- sources/parsing/description_check.c
+++ sources/parsing/description_check.c
@@ -96,11 +96,11 @@ int	is_valid_description(char *str)
 	if (ft_strnstr(str, "NO", len))
 		return (1);
 	else if (ft_strnstr(str, "SO", 2))
-		return (2);
-	else if (ft_strnstr(str, "WE", 2))
 		return (3);
-	else if (ft_strnstr(str, "EA", 2))
+	else if (ft_strnstr(str, "WE", 2))
 		return (4);
+	else if (ft_strnstr(str, "EA", 2))
+		return (2);
 	if (len > 1)
 		return (0);
 	else if (ft_strnstr(str, "F", 1))
